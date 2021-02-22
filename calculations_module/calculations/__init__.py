5e0e13b202fe595e9705f88739dbd64443294d3f
Jorewin <blazejowski.j@gmail.com>
2021-02-22 20:02:25 +0100

    Calculations module initialization

---
+++ calculations_module/calculations/__init__.py
@@ -0,0 +1,10 @@
+from .utilities import (
+    generate_constants,
+    generate_parameters,
+    import_data,
+    data_iterator
+)
+
+
+__version__ = "0.0.1"
+__authors__ = ["Bartłomiej Banachowicz", "Jakub Błażejowski", "Julia Bielecka"]
